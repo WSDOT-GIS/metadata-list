d9e104e0c6b4c94b44859d5f0ed4171acbf3aad0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-05-01 11:47:54 -0700

    refactor: moved DOM functions from demo to metadata-list

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -1,95 +1,93 @@
-import { getMapServiceMetadata, IMetadataDocList } from "@wsdot/metadata-list";
+import { createList, getWebMap } from "@wsdot/metadata-list";
 
-import { getItemData } from "@esri/arcgis-rest-items";
+// interface IOperationalLayer {
+//   id: string;
+//   title: string;
+//   url: string;
+//   [key: string]: any;
+// }
 
-interface IOperationalLayer {
-  id: string;
-  title: string;
-  url: string;
-  [key: string]: any;
-}
+// interface IWebMap {
+//   [key: string]: any;
+//   operationalLayers: IOperationalLayer[];
+// }
 
-interface IWebMap {
-  [key: string]: any;
-  operationalLayers: IOperationalLayer[];
-}
+// async function getWebMap(mapId: string) {
+//   const webmap: IWebMap = await getItemData(mapId, {
+//     httpMethod: "GET"
+//   });
+//   return webmap;
+// }
 
-async function getWebMap(mapId: string) {
-  const webmap: IWebMap = await getItemData(mapId, {
-    httpMethod: "GET"
-  });
-  return webmap;
-}
+// /**
+//  * Creates a list item that will show metadata links for data
+//  * that is displayed by an operational layer.
+//  * @param layer An operational layer.
+//  */
+// function createListItem(layer: IOperationalLayer): HTMLLIElement {
+//   const li = document.createElement("li");
+//   li.textContent = layer.title;
+//   const progress = document.createElement("progress");
+//   li.appendChild(progress);
 
-/**
- * Creates a list item that will show metadata links for data
- * that is displayed by an operational layer.
- * @param layer An operational layer.
- */
-function createListItem(layer: IOperationalLayer): HTMLLIElement {
-  const li = document.createElement("li");
-  li.textContent = layer.title;
-  const progress = document.createElement("progress");
-  li.appendChild(progress);
+//   getMapServiceMetadata(layer.url).then(
+//     docList => {
+//       // setup the links and remove progress bar.
+//       progress.remove();
+//       if (!docList) {
+//         const message = document.createElement("p");
+//         message.textContent = "No metadata available for this layer";
+//         li.appendChild(message);
+//         return;
+//       }
 
-  getMapServiceMetadata(layer.url).then(
-    docList => {
-      // setup the links and remove progress bar.
-      progress.remove();
-      if (!docList) {
-        const message = document.createElement("p");
-        message.textContent = "No metadata available for this layer";
-        li.appendChild(message);
-        return;
-      }
+//       const innnerList = document.createElement("ul");
 
-      const innnerList = document.createElement("ul");
+//       for (const datasetName in docList) {
+//         if (docList.hasOwnProperty(datasetName)) {
+//           const url = docList[datasetName];
+//           const innerLI = document.createElement("li");
+//           const a = document.createElement("a");
+//           a.href = url;
+//           a.textContent = datasetName;
+//           a.target = "_blank";
+//           innerLI.appendChild(a);
+//           innnerList.appendChild(innerLI);
+//         }
+//       }
 
-      for (const datasetName in docList) {
-        if (docList.hasOwnProperty(datasetName)) {
-          const url = docList[datasetName];
-          const innerLI = document.createElement("li");
-          const a = document.createElement("a");
-          a.href = url;
-          a.textContent = datasetName;
-          a.target = "_blank";
-          innerLI.appendChild(a);
-          innnerList.appendChild(innerLI);
-        }
-      }
+//       li.appendChild(innnerList);
+//     },
+//     error => {
+//       // remove progress and add error message info.
+//       progress.remove();
+//       const textNode = document.createTextNode(
+//         error.message || "No metadata available"
+//       );
+//       li.appendChild(textNode);
+//     }
+//   );
 
-      li.appendChild(innnerList);
-    },
-    error => {
-      // remove progress and add error message info.
-      progress.remove();
-      const textNode = document.createTextNode(
-        error.message || "No metadata available"
-      );
-      li.appendChild(textNode);
-    }
-  );
+//   return li;
+// }
 
-  return li;
-}
+// /**
+//  * Creates a list of all the metadata for the data associated with the operational layers in a web map.
+//  * @param webmap A web map
+//  */
+// function createList(webmap: IWebMap) {
+//   const { operationalLayers } = webmap;
+//   const frag = document.createDocumentFragment();
 
-/**
- * Creates a list of all the metadata for the data associated with the operational layers in a web map.
- * @param webmap A web map
- */
-function createList(webmap: IWebMap) {
-  const { operationalLayers } = webmap;
-  const frag = document.createDocumentFragment();
+//   operationalLayers.map(createListItem).forEach(li => {
+//     frag.appendChild(li);
+//   });
 
-  operationalLayers.map(createListItem).forEach(li => {
-    frag.appendChild(li);
-  });
+//   const list = document.createElement("ul");
+//   list.appendChild(frag);
 
-  const list = document.createElement("ul");
-  list.appendChild(frag);
-
-  return list;
-}
+//   return list;
+// }
 
 /**
  * Starts the code for the demo page.
--- packages/metadata-list/src/index.ts
+++ packages/metadata-list/src/index.ts
@@ -1,3 +1,4 @@
+import { getItemData } from "@esri/arcgis-rest-items";
 /**
  * Regular expression.
  * 0. Matched string
@@ -200,4 +201,93 @@ export async function getMapServiceMetadata(url: string) {
 
   const txt = await response.text();
   return JSON.parse(txt, sourceReviver) as IMetadataDocList;
+}
+
+export interface IOperationalLayer {
+  id: string;
+  title: string;
+  url: string;
+  [key: string]: any;
+}
+
+export interface IWebMap {
+  [key: string]: any;
+  operationalLayers: IOperationalLayer[];
+}
+
+export async function getWebMap(mapId: string) {
+  const webmap: IWebMap = await getItemData(mapId, {
+    httpMethod: "GET"
+  });
+  return webmap;
+}
+
+/**
+ * Creates a list item that will show metadata links for data
+ * that is displayed by an operational layer.
+ * @param layer An operational layer.
+ */
+function createListItem(layer: IOperationalLayer): HTMLLIElement {
+  const li = document.createElement("li");
+  li.textContent = layer.title;
+  const progress = document.createElement("progress");
+  li.appendChild(progress);
+
+  getMapServiceMetadata(layer.url).then(
+    docList => {
+      // setup the links and remove progress bar.
+      progress.remove();
+      if (!docList) {
+        const message = document.createElement("p");
+        message.textContent = "No metadata available for this layer";
+        li.appendChild(message);
+        return;
+      }
+
+      const innnerList = document.createElement("ul");
+
+      for (const datasetName in docList) {
+        if (docList.hasOwnProperty(datasetName)) {
+          const url = docList[datasetName];
+          const innerLI = document.createElement("li");
+          const a = document.createElement("a");
+          a.href = url;
+          a.textContent = datasetName;
+          a.target = "_blank";
+          innerLI.appendChild(a);
+          innnerList.appendChild(innerLI);
+        }
+      }
+
+      li.appendChild(innnerList);
+    },
+    error => {
+      // remove progress and add error message info.
+      progress.remove();
+      const textNode = document.createTextNode(
+        error.message || "No metadata available"
+      );
+      li.appendChild(textNode);
+    }
+  );
+
+  return li;
+}
+
+/**
+ * Creates a list of all the metadata for the data associated with the operational layers in a web map.
+ * @param webmap A web map
+ */
+export function createList(webmap: IWebMap) {
+  const { operationalLayers } = webmap;
+  const frag = document.createDocumentFragment();
+
+  operationalLayers.map(createListItem).forEach(li => {
+    frag.appendChild(li);
+  });
+
+  const list = document.createElement("ul");
+  list.appendChild(frag);
+
+  return list;
 }
